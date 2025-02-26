e9c01e007f2a85d24d5b06d7ac77bed95044c4d4
Mzlf <mimimisha2110@gmail.com>
2025-02-26 19:14:40 +0300

    Co-authored-by: Wizard Today <wizard-today@users.noreply.github.com>

--- src/start.ts
+++ src/start.ts
@@ -1,4 +1,4 @@
-import { Telegraf } from 'telegraf'
+import { Telegraf, Markup, Context } from 'telegraf'
 import { message } from 'telegraf/filters'
 import { randomInt } from 'node:crypto' // min <= n < max
 import dotenv from 'dotenv'
@@ -11,34 +11,74 @@ if (!BOT_TOKEN) {
   throw 'Добавьте BOT_TOKEN в .env'
 }
 
-const userNumber: { [key: number]: number } = {}
+const userNumber: { [key: string]: number } = {}
+
 const bot = new Telegraf(BOT_TOKEN)
 
 bot.start((ctx) => ctx.reply('Welcome'))
 bot.help((ctx) => ctx.reply('Send me a sticker'))
 
-let randomNumber = randomInt(1, 10 + 1)
+// /log
+bot.command('log', () => {
+  console.log(userNumber)
+})
 
-// 3
-// 1 2 ... 10
-// 3 -> 1
+//  241912341
+// 7065183833
+
+// for (let n = 0; n <= 11; n++) {
+//   bot.action(String(n), async (ctx) => {
+//     await ctx.answerCbQuery()
+//     const chatId = ctx.chat!.id
+//     await answer(ctx, chatId, n)
+//   })
+// }
+
+// RegExp: https://learn.javascript.ru/regular-expressions
+
+bot.action(/^\d+$/, async (ctx) => {
+  await ctx.answerCbQuery()
+  const chatId = ctx.chat!.id
+  const n = Number(ctx.match[0])
+  await answer(ctx, chatId, n)
+})
 
 bot.on(message('text'), async (ctx) => {
-  const chatId = ctx.message.chat.id
-  ctx.chat.id
-  const n = Number(ctx.message.text)
-  if (n === randomNumber) {
-    randomNumber = randomInt(1, 10 + 1)
-    userNumber[chatId] = randomNumber
-    await ctx.reply('Ты угадал')
-  } 
-  else if (n < randomNumber) {
-    ctx.reply('Мое число больше.');
-  } else {
-    ctx.reply('Мое число меньше.');
+  const chatId = ctx.chat.id
+  // ctx.message.from.username
+
+  if (!(chatId in userNumber)) {
+    await ctx.reply(
+      `Привет, ${ctx.message.from.username}! Я загадал число, попробуй угадать) `+
+      `Если не угодаешь то ты станешь чилом)`
+    )
+    userNumber[chatId] = randomInt(1, 10 + 1)
   }
+  
+  const n = Number(ctx.message.text)
+  await answer(ctx, chatId, n)
 })
 
 bot.launch(() => {
   console.log('Bot started 🚀')
 })
+
+async function answer(ctx: Context, chatId: number, n: number) {
+  if (!isFinite(n)){
+    await ctx.reply('Ты читер! Нужно ввести число от 1 до 10')
+  }
+  else if (n === userNumber[chatId]) {
+    const randomNumber = randomInt(1, 10 + 1)
+    userNumber[chatId] = randomNumber
+    await ctx.reply('Ты угадал')
+  }
+  else {
+    await ctx.reply(
+      n < userNumber[chatId]! ? 'Мое число больше.' : 'Мое число меньше.', 
+      Markup.inlineKeyboard([
+        [Markup.button.callback(String(n+1), String(n+1))],
+        [Markup.button.callback(String(n-1), String(n-1))],
+      ])
+    )
+  }
+}
